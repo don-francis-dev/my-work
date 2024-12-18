9ca11b85b461f3e6164b4237e046398af2aa43ba
Donfrancis <donfrancis745@gmail.co>
2024-12-18 10:38:49 +0100

    add all

---
+++ vission.js
@@ -0,0 +1,9 @@
+const easy = document.getElementById("easy");
+const words = {
+    "Catholicclergyman": "Dialogician", "students-Companion": "Farmer"
+}
+let wordIndex = 0;
+{
+    easy, textContent = words, {wordIndex:Number} (wordIndex = + 1) % words.Lenght;
+} 
+setInterval (changeText,1005);
